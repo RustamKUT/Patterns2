2c65bc4fe9c621b43742a55cbd70a54b8ca62bf7
Rustam <kutrustam1991@gmail.com>
2025-09-29 18:27:03 +0500

    commit5

---
+++ src/test/java/netology/ru/data/RegistrationInfo.java
@@ -0,0 +1,28 @@
+package netology.ru.data;
+
+import lombok.Data;
+
+@Data
+public class RegistrationInfo {
+    private final String login;
+    private final String password;
+    private final String status;
+
+    public RegistrationInfo(String login, String password, String status) {
+        this.login = login;
+        this.password = password;
+        this.status = status;
+    }
+
+    public String getLogin() {
+        return null;
+    }
+
+    public String getPassword() {
+        return null;
+    }
+
+    public String getStatus() {
+        return null;
+    }
+}
