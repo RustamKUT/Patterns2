ce94cd1d9239761454ec64f9a569c1e09bc1a01b
Rustam <kutrustam1991@gmail.com>
2025-10-10 00:40:22 +0500

    commit17

--- src/test/java/netology/ru/data/RegistrationInfo.java
+++ src/test/java/netology/ru/data/RegistrationInfo.java
@@ -4,37 +4,9 @@ import lombok.Data;
 
 @Data
 public class RegistrationInfo {
-    private String login;
-    private String password;
-    private String status;
+    private final String login;
+    private final String password;
+    private final String status;
 
-    public RegistrationInfo(String login, String password, String status) {
-        this.login = login;
-        this.password = password;
-        this.status = status;
-    }
 
-    public String getLogin() {
-        return login;
-    }
-
-    public void setLogin(String login) {
-        this.login = login;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
 }
